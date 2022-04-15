10be57a82f9b0ec3d5db9357e5bd99eb40419cc4
Andre Enes <andre.ferreira.enes@gmail.com>
2022-04-15 16:41:29 +0100

    test

--- models/connection.js
+++ models/connection.js
@@ -1,14 +1,16 @@
 var pg = require('pg');
 
 const connectionString = process.env.DATABASE_URL
+let print={name:connectionString};
+send(print);
 const Pool = pg.Pool
 const pool = new Pool({
     connectionString,
-    max: 10 ,
-      ssl: {
-          require: true, 
-          rejectUnauthorized: false
-      }
+    max: 10//,
+     // ssl: {
+       //   require: true, 
+         // rejectUnauthorized: false
+     // }
 })
 
 module.exports = pool;
